5cb8ee92e9aec9f312bab8e04acc533371fbb01c
robielcpnv <89458745+robielcpnv@users.noreply.github.com>
2022-01-14 09:03:33 +0100

    add gradle

---
+++ app/src/main/java/CPNV/what_the_dog/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package CPNV.what_the_dog.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
