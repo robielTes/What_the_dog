efa91a72e0351ebecc89bdcd419e0c16efbdb851
TGR <78166079+TGACPNV@users.noreply.github.com>
2022-05-11 15:01:24 +0200

    start to create db relations

--- app/src/main/java/CPNV/what_the_dog/model/Client.kt
+++ app/src/main/java/CPNV/what_the_dog/model/Client.kt
@@ -1,4 +1,20 @@
 package CPNV.what_the_dog.model
 
 class Client {
+    var id: Int = 0
+    var firstname: String = ""
+    var lastname: String = ""
+    var gender: String = ""
+    var email: String = ""
+    var phone: String = ""
+    var address: String = ""
+    var city: String = ""
+    var state: String = ""
+    var zip: String = ""
+    var country: String = ""
+
+    constructor() {
+
+    }
+
 }
