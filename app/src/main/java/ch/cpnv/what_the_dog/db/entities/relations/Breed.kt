66fb5eea5de8dfbe64a0c1f66c2fd83fdbf1aded
robielcpnv <robiel.tesfazghi@cpnv.ch>
2022-06-01 14:34:14 +0200

    Create Dog Breed entity

--- app/src/main/java/ch/cpnv/what_the_dog/db/entities/relations/Breed.kt
+++ app/src/main/java/ch/cpnv/what_the_dog/db/entities/relations/Breed.kt
@@ -1,4 +1,25 @@
 package ch.cpnv.what_the_dog.db.entities.relations
 
-class Breed {
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "breeds")
+data class Breed (
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    @ColumnInfo(name = "name") val name: String,
+    @ColumnInfo(name = "link") val link: String?,
+    @ColumnInfo(name = "description") val description: String?,
+    @ColumnInfo(name = "picture") val picture: String?,
+    @ColumnInfo(name = "classification") val classification: String?,
+    @ColumnInfo(name = "min_size_female") val minimumSizeFemale: Int?,
+    @ColumnInfo(name = "max_size_female") val maximumSizeFemale: Int?,
+    @ColumnInfo(name = "min_size_male") val minimumSizeMale: Int?,
+    @ColumnInfo(name = "max_size_male") val maximumSizeMale: Int?,
+    @ColumnInfo(name = "min_weight_female") val minimumWeightFemale: Int?,
+    @ColumnInfo(name = "max_weight_female") val maximumWeightFemale: Int?,
+    @ColumnInfo(name = "min_weight_male") val minimumWeightMale: Int?,
+    @ColumnInfo(name = "max_weight_male") val maximumWeightMale: Int?,
+    @ColumnInfo(name = "life_expectancy") val lifeExpectancy: Int?
+) {
 }
