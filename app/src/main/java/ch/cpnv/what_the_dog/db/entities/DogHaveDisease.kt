3bfd00e82ff940f2ce82581336d2d2c5dbe1ce06
Noah <noah.delgado@cpnv.ch>
2022-06-01 14:51:50 +0200

    Add DogHaveDisease entitie

---
+++ app/src/main/java/ch/cpnv/what_the_dog/db/entities/DogHaveDisease.kt
@@ -0,0 +1,13 @@
+package ch.cpnv.what_the_dog.db.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "dogs_have_diseases")
+data class DogHaveDisease(
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id") val id: Int,
+    @ColumnInfo(name = "id_dog") val idDog: Int,
+    @ColumnInfo(name = "id_disease") val idDisease: Int
+    ){
+}
