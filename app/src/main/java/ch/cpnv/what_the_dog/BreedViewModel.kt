d7179878b6b80fc47232f80c4882daad1f11bc1d
robiel.tesfazghi@cpnv.ch <robiel.tesfazghi@cpnv.ch>
2022-06-21 23:16:33 +0200

    Add ViewModel for breed

---
+++ app/src/main/java/ch/cpnv/what_the_dog/BreedViewModel.kt
@@ -0,0 +1,49 @@
+package ch.cpnv.what_the_dog
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import ch.cpnv.what_the_dog.db.CynoDatabase
+import ch.cpnv.what_the_dog.db.entities.Breed
+import ch.cpnv.what_the_dog.model.BreedRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+
+class BreedViewModel(application: Application) :AndroidViewModel(application){
+    private val getBreeds: LiveData<List<Breed>>
+    private val repository: BreedRepository
+
+    init {
+        val breedDao = CynoDatabase.getDatabase(application, CoroutineScope(SupervisorJob())).breedDao()
+        repository = BreedRepository(breedDao)
+        getBreeds = repository.getBreeds
+    }
+
+    fun addBreed(breed: Breed){
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.addBreed(breed)
+        }
+    }
+
+    fun getBreed(id: Int){
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.getBreed(id)
+        }
+    }
+
+    fun updateBreed(breed: Breed){
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.updateBreed(breed)
+        }
+    }
+
+    fun deleteBreed(breed: Breed){
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.updateBreed(breed)
+        }
+    }
+
+}
--- app/src/main/java/ch/cpnv/what_the_dog/db/entities/relations/DogWithBreed.kt
+++ app/src/main/java/ch/cpnv/what_the_dog/db/entities/relations/DogWithBreed.kt
@@ -1,11 +1,13 @@
 package ch.cpnv.what_the_dog.db.entities.relations
 
+import CPNV.what_the_dog.db.entities.Disease
 import androidx.room.Embedded
 import androidx.room.Relation
+import ch.cpnv.what_the_dog.db.entities.Breed
 import ch.cpnv.what_the_dog.db.entities.Dog
 
 data class DogWithBreed(
     @Embedded val dog: Dog,
     @Relation(parentColumn = "breed_id", entityColumn = "id")
-
+    val diseases: List<Breed>
     ){}
