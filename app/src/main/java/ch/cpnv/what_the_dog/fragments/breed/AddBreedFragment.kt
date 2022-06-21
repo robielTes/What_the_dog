e0f534c1c4c3edc89c516c084d1579006868d68f
robiel.tesfazghi@cpnv.ch <robiel.tesfazghi@cpnv.ch>
2022-06-22 00:12:06 +0200

    Add add_breed_fragment

---
+++ app/src/main/java/ch/cpnv/what_the_dog/fragments/breed/AddBreedFragment.kt
@@ -0,0 +1,20 @@
+package ch.cpnv.what_the_dog.fragments.breed
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ch.cpnv.what_the_dog.R
+
+class AddBreedFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_breed, container, false)
+    }
+
+}
